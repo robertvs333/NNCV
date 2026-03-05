41948774ef3b3c20cb5934a95920eddf42319769
Robert van Straten <scur2425@int4.local.snellius.surf.nl>
2026-03-05 17:18:01 +0100

    trained model

--- Final assignment/download_docker_and_data.sh
+++ Final assignment/download_docker_and_data.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=9
 #SBATCH --gpus=1
 #SBATCH --partition=gpu_mig
-#SBATCH --time=3:00:00
+#SBATCH --time=0:10:00
 
 # Pull container from dockerhub
 apptainer pull container.sif docker://cclaess/5lsm0:v1
