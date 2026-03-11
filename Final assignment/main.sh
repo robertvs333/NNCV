8f44a1bf39f126ae36efef52c8d7460285a71afa
robert van straten <robertjanvanstraten333@gmail.com>
2026-03-11 16:43:22 +0100

    optimized model

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -3,7 +3,7 @@ wandb login
 python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
-    --epochs 100 \
+    --epochs 40 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
